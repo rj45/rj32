d908109a0d811eccb73ad7d0c9db54c497d838f2
Ryan Sanche <ryan@rj45.ca>
2021-03-29 14:14:46 -0700

    Turing complete! Load Store!

--- programs/cpudef.asm
+++ programs/cpudef.asm
@@ -8,18 +8,21 @@
 }
 
 #ruledef {
-  add  {rd:reg}, {value}    =>   value`5 @  0`4 @ rd`4 @ 0b011
-  add  {rd:reg}, {rs:reg}   => 1`1 @ 0`4 @ rs`4 @ rd`4 @ 0b010
-  move {rd:reg}, {value}    =>   value`5 @  6`4 @ rd`4 @ 0b011
-  move {rd:reg}, {rs:reg}   => 1`1 @ 6`4 @ rs`4 @ rd`4 @ 0b010
-  eq   {rd:reg}, {value}    =>   value`5 @  4`4 @ rd`4 @ 0b011
-  eq   {rd:reg}, {rs:reg}   => 1`1 @ 4`4 @ rs`4 @ rd`4 @ 0b010
-  jump {value}              =>  value`11 @         0`2 @ 0b001
-  jump {rs:reg}             => 0`1 @ 4`4 @  0`4 @ rd`4 @ 0b010
-  brt  {value}              =>  value`11 @         2`2 @ 0b001
-  brt  {rs:reg}             => 0`1 @ 4`4 @  2`4 @ rd`4 @ 0b010
-  brf  {value}              =>  value`11 @         3`2 @ 0b001
-  brf  {rs:reg}             => 0`1 @ 4`4 @  3`4 @ rd`4 @ 0b010
+  add   {rd:reg}, {value}           =>   value`5 @  0`4 @ rd`4 @ 0b011
+  add   {rd:reg}, {rs:reg}          => 1`1 @ 0`4 @ rs`4 @ rd`4 @ 0b010
+  move  {rd:reg}, {value}           =>   value`5 @  6`4 @ rd`4 @ 0b011
+  move  {rd:reg}, {rs:reg}          => 1`1 @ 6`4 @ rs`4 @ rd`4 @ 0b010
+  eq    {rd:reg}, {value}           =>   value`5 @  4`4 @ rd`4 @ 0b011
+  eq    {rd:reg}, {rs:reg}          => 1`1 @ 4`4 @ rs`4 @ rd`4 @ 0b010
+  jump  {value}                     =>  value`11 @         0`2 @ 0b001
+  jump  {rs:reg}                    => 0`1 @ 4`4 @  0`4 @ rd`4 @ 0b010
+  brt   {value}                     =>  value`11 @         2`2 @ 0b001
+  brt   {rs:reg}                    => 0`1 @ 4`4 @  2`4 @ rd`4 @ 0b010
+  brf   {value}                     =>  value`11 @         3`2 @ 0b001
+  brf   {rs:reg}                    => 0`1 @ 4`4 @  3`4 @ rd`4 @ 0b010
 
-  halt                      => 0`11             @  1`2 @ 0b000
+  load  {rd:reg}, [{rs:reg}, {imm}] =>     imm`5 @ rs`4 @ rd`4 @ 0b100
+  store [{rs:reg}, {imm}], {rd:reg} =>     imm`5 @ rs`4 @ rd`4 @ 0b101
+
+  halt                              => 0`11             @  1`2 @ 0b000
 }
--- programs/fib.asm
+++ programs/fib.asm
@@ -1,14 +1,21 @@
 #include "cpudef.asm"
 
 fibonacci:
+  move r0, 0
   move r1, 0
   move r2, 1
   .loop:
     add r2, r1
     move r3, r2
+
+    store [r0, 0], r3
+
     add r1, r2
     move r3, r1
-    eq r3, 8
-    brt fibonacci
+
+    store [r0, 1], r3
+    load r3, [r0,0]
+    add r0, 2
+
     jump .loop
 
