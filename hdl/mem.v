cc0da1b3574bb147ab1334db427868b65d3ba0dd
Ryan Sanche <ryan@rj45.ca>
2021-05-12 05:22:47 -0700

    VGA front panel working well

--- hdl/mem.v
+++ hdl/mem.v
@@ -1,4 +1,4 @@
-module bram (
+module prog_bram (
     input             clk_i,
     input       [7:0] addr_i,
     output reg [15:0] data_o
@@ -9,6 +9,22 @@ reg [15:0] mem [0:255];
 initial $readmemh("./test.hex",mem);
 
 always @(negedge clk_i) begin
+    data_o <= mem[addr_i];
+end
+
+endmodule
+
+module font_bram (
+    input             clk_i,
+    input      [10:0] addr_i,
+    output reg [15:0] data_o
+);
+
+reg [15:0] mem [0:2047];
+
+initial $readmemh("./font16seg.hex",mem);
+
+always @(posedge clk_i) begin
     data_o <= mem[addr_i];
 end
 
--- hdl/top.v
+++ hdl/top.v
@@ -13,7 +13,15 @@ module top (
   output P2_7 ,
   output P2_8 ,
   output P2_9 ,
-  output P2_10
+  output P2_10,
+  output P3_1 ,
+  output P3_2 ,
+  output P3_3 ,
+  output P3_4 ,
+  output P3_7 ,
+  output P3_8 ,
+  output P3_9 ,
+  output P3_10
 );
 
   reg [15:0] D_prog;
@@ -32,8 +40,9 @@ module top (
   wire rd_valid;
   wire rs_valid;
   wire [2:0] cond;
-  wire [15:0] L;
-  wire [15:0] R;
+  wire [15:0] L_b;
+  wire [15:0] R_b;
+  wire [15:0] result;
   wire [1:0] rd;
   wire [1:0] rs;
   wire jump;
@@ -52,19 +61,33 @@ module top (
   wire green;
   wire blue;
 
+  assign red = 0;
+  assign green = 0;
+  assign blue = 0;
+
   assign nred = ~red;
   assign ngreen = ~green;
   assign nblue = ~blue;
 
+  wire [3:0] r;
+  wire [3:0] g;
+  wire [3:0] b;
+  wire vga_hs;
+  wire vga_vs;
+  wire vga_de;
+
+  wire [10:0] fontA;
+  reg [15:0] fontD;
+
   debouncer step_deb(
-    .i_clk(clock),
+    .i_clk(clk_12m),
     .i_in(~step),
     .o_debounced(step_debounced),
     .o_debug(db1)
   );
 
   debouncer run_deb(
-    .i_clk(clock),
+    .i_clk(clk_12m),
     .i_in(~run),
     .o_debounced(run_debounced),
     .o_debug(db2)
@@ -72,7 +95,7 @@ module top (
 
   rj32 cpu(
     //inputs
-    .clock(clock),
+    .clock(clk_12m),
     .step(step_debounced),
     .run(run_debounced),
     .D_prog(D_prog),
@@ -84,23 +107,21 @@ module top (
     .R2(R2),
     .R3(R3),
     .PC(PC),
-    // .halt(halt),
-    .halt(green),
+    .halt(halt),
 
     .error(error),
-    // .skip(skip),
-    .skip(red),
+    .skip(skip),
     .rd_valid(rd_valid),
     .rs_valid(rs_valid),
     .op(op),
     .cond(cond),
     .stall(stall),
-    .L(L),
-    .R(R),
+    .L(L_b),
+    .R(R_b),
+    .result(result),
     .rd(rd),
     .rs(rs),
-    // .jump(jump),
-    .jump(blue),
+    .jump(jump),
     .A_prog(A_prog),
     .clock_m(nclock),
     .A_data(A_data),
@@ -108,41 +129,159 @@ module top (
     .w_en(w_en),
   );
 
+  wire n_clk_12m;
+
+  assign n_clk_12m = ~clk_12m;
+
   SB_SPRAM256KA dataram (
     .ADDRESS(A_data),
     .DATAIN(D_out),
     .MASKWREN({w_en, w_en, w_en, w_en}),
     .WREN(w_en),
     .CHIPSELECT(1'b1),
-    .CLOCK(~clock),
+    .CLOCK(n_clk_12m),
     .STANDBY(1'b0),
     .SLEEP(1'b0),
     .POWEROFF(1'b1),
     .DATAOUT(D_in)
   );
 
-  bram progmem (
-    .clk_i(clock),
+  prog_bram progmem (
+    .clk_i(clk_12m),
     .addr_i(A_prog),
     .data_o(D_prog),
   );
 
-  reg [31:0] cnt;
-  always @(posedge clock) cnt <= cnt+1;
+  `ifdef RES_720x400
+  localparam [11:0] res_H = 720;
+  localparam [11:0] fp_H = 36;
+  localparam [11:0] sync_H = 72;
+  localparam [11:0] bp_H = 108;
+  localparam neg_H = 1;
+  localparam [11:0] res_V = 400;
+  localparam [11:0] fp_V = 1;
+  localparam [11:0] sync_V = 3;
+  localparam [11:0] bp_V = 42;
+  localparam neg_V = 0;
+  // 12 MHz -> 35.25 MHz (goal: 35.5 MHz)
+  localparam divf = 7'b0101110; // 46
+  localparam divq = 3'b100; // 4
+`elsif RES_720x480
+  localparam [11:0] res_H = 720;
+  localparam [11:0] fp_H = 16;
+  localparam [11:0] sync_H = 62;
+  localparam [11:0] bp_H = 60;
+  localparam neg_H = 1;
+  localparam [11:0] res_V = 480;
+  localparam [11:0] fp_V = 9;
+  localparam [11:0] sync_V = 6;
+  localparam [11:0] bp_V = 30;
+  localparam neg_V = 1;
+  // 12 MHz -> 27.00 MHz (goal: 27.00 MHz)
+  localparam divf = 7'b1000111; // 71
+  localparam divq = 3'b101; // 5
+`else // 640x480
+  localparam [11:0] res_H = 640;
+  localparam [11:0] fp_H = 16;
+  localparam [11:0] sync_H = 96;
+  localparam [11:0] bp_H = 48;
+  localparam neg_H = 1;
+  localparam [11:0] res_V = 480;
+  localparam [11:0] fp_V = 10;
+  localparam [11:0] sync_V = 2;
+  localparam [11:0] bp_V = 33;
+  localparam neg_V = 1;
+  // 12 MHz -> 25.125 MHz (goal: 25.175 MHz)
+  localparam divf = 7'b1000010; // 66
+  localparam divq = 3'b101; // 5
+`endif
 
-  display r3out (
-    .CLK(clock),
-    // .byt({3'b0, op[0:4]}),
-    // .byt(PC[7:0]),
-    .byt(R3[7:0]),
-    .P2_1(P2_1),
-    .P2_2(P2_2),
-    .P2_3(P2_3),
-    .P2_4(P2_4),
-    .P2_7(P2_7),
-    .P2_8(P2_8),
-    .P2_9(P2_9),
-    .P2_10(P2_10)
+  // 12 MHz -> 25.125 MHz (goal: 25.175 MHz)
+  SB_PLL40_2F_PAD #(
+  // SB_PLL40_CORE #(
+		.FEEDBACK_PATH("SIMPLE"),
+		.DIVR(4'b0000),		// DIVR =  0
+		.DIVF(divf),	// DIVF = ?
+		.DIVQ(divq),		// DIVQ =  ?
+		.FILTER_RANGE(3'b001),	// FILTER_RANGE = 1
+    .PLLOUT_SELECT_PORTB("GENCLK_HALF"),
+	) uut (
+		.RESETB(1'b1),
+		.BYPASS(1'b0),
+		.PACKAGEPIN(clock),
+    //.REFERENCECLK(clock),
+
+		.PLLOUTGLOBALA(clk_25m),
+    .PLLOUTGLOBALB(clk_12m),
   );
+
+  font_bram fontmem (
+    .clk_i(clk_25m),
+    .addr_i(fontA),
+    .data_o(fontD),
+  );
+
+  vga_blinkenlights vgafrontpanel(
+    .clock(clk_25m),
+    .clock_slow(clk_12m),
+
+    // .res_H(res_H),
+    // .fp_H(fp_H),
+    // .sync_H(sync_H),
+    // .bp_H(bp_H),
+    // .neg_H(neg_H),
+    // .res_V(res_V),
+    // .fp_V(fp_V),
+    // .sync_V(sync_V),
+    // .bp_V(bp_V),
+    // .neg_V(neg_V),
+
+    .PC(PC),
+    .R0(R0),
+    .R1(R1),
+    .R2(R2),
+    .R3(R3),
+    .L_b(L_b),
+    .R_b(R_b),
+    .result(result),
+    .skip(skip),
+    .jump(jump),
+    .op(op),
+    .cond(cond),
+
+    .CD(fontD),
+
+    .R(r),
+    .G(g),
+    .B(b),
+    .hs(vga_hs),
+    .vs(vga_vs),
+    .de(vga_de),
+    .CA(fontA)
+  );
+
+  assign {P2_1,   P2_2,   P2_3,   P2_4,   P2_7,   P2_8,   P2_9,   P2_10} =
+         {r[3],   r[1],   g[3],   g[1],   r[2],   r[0],   g[2],   g[0]};
+  assign {P3_1,   P3_2,   P3_3,   P3_4,   P3_7,   P3_8,   P3_9,   P3_10} =
+         {b[3],   clk_25m,b[0],   vga_hs, b[2],   b[1],   vga_de, vga_vs};
+
+
+  // reg [31:0] cnt;
+  // always @(posedge clock) cnt <= cnt+1;
+
+  // display r3out (
+  //   .CLK(clock),
+  //   // .byt({3'b0, op[0:4]}),
+  //   // .byt(PC[7:0]),
+  //   .byt(R3[7:0]),
+  //   .P2_1(P2_1),
+  //   .P2_2(P2_2),
+  //   .P2_3(P2_3),
+  //   .P2_4(P2_4),
+  //   .P2_7(P2_7),
+  //   .P2_8(P2_8),
+  //   .P2_9(P2_9),
+  //   .P2_10(P2_10)
+  // );
 
 endmodule
