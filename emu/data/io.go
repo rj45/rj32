d9327173acaadb608cad651ca4c808f2e9c2ef48
Ryan Sanche <ryan@rj45.ca>
2021-10-31 06:40:11 -0700

    Attempt to emulate serial terminal in emulator

--- emu/data/io.go
+++ emu/data/io.go
@@ -10,9 +10,12 @@ func stdoutWriter(bus Bus) Bus {
 	if bus.WE() {
 		char := bus.Data()
 		buf := []byte{byte(char)}
-		if char > 127 {
-			buf = append(buf, byte(char>>8))
+
+		// emulate serial/telnet handling of LF
+		if buf[0] == 10 {
+			buf = []byte{033, 'D'}
 		}
+
 		_, err := os.Stdout.Write(buf)
 		if err != nil {
 			panic(err)
